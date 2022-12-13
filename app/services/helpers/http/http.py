8d701ea102e674adb15c12d3626d15ef1b1c5107
buguno <brunnodesouzabezerra@gmail.com>
2022-12-13 18:33:42 -0300

    feat: creating http helper

---
+++ app/services/helpers/http/http.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+from typing import Any
+
+
+class HttpResponse:
+    def __init__(self, status_code: int, body: Any=None):
+        self.status_code = status_code
+        self.body = body
+
+    def __repr__(self):
+        return f"HttpResponse (status_code={self.status_code}, body={self.body})"
+
+
+class HttpStatus:
+    """Status code and body for HTTP responses, following the https://http.cat
+
+    Returns:
+        HttpResponse: status_code and body
+    """
+    @staticmethod
+    def ok_200(body: Any) -> HttpResponse:
+        return HttpResponse(HTTPStatus.OK, body)
