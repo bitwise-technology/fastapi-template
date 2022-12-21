f69e3c53ebb03ab6cd0a8bc1b8eef78b88d0da07
buguno <brunnodesouzabezerra@gmail.com>
2022-12-21 17:21:48 -0300

    fix: fixing decorator

--- app/main/config/environment.py
+++ app/main/config/environment.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 from pydantic import BaseSettings
 
 
-@lru_cache
+@lru_cache()
 def get_env_filename():
     runtime_env = os.getenv("ENV")
     return f".env.{runtime_env}" if runtime_env else ".env"
@@ -19,6 +19,6 @@ class EnvironmentSettings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-@lru_cache
+@lru_cache()
 def get_environment_variables():
     return EnvironmentSettings()
