8432ce368bec9b744b910d376431990bf0101fa6
buguno <brunnodesouzabezerra@gmail.com>
2022-12-27 18:03:43 -0300

    refactor: adding adapter call and return body

--- app/main/routes/hello_routes.py
+++ app/main/routes/hello_routes.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from typing import Union
+from fastapi import APIRouter, Response
+from app.main.adapters import fast_api_adapter
+from app.main.factories import hello_world_factory
+from app.domain.usecases.hello_world import HelloParams
+
 
 HelloWorld = APIRouter(prefix="/hello", tags=["Hello World"])
 
 
 @HelloWorld.get("/")
-def hello_world():
-    return {"Hello": "World"}
+def hello_world(body: HelloParams, response: Response):
+    result = fast_api_adapter(body, hello_world_factory())
+    response.status_code = result.status_code
+    return result.body
