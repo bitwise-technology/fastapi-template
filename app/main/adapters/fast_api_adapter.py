c7bdd165f36fe69f319299aeb89374b609df129d
buguno <brunnodesouzabezerra@gmail.com>
2023-01-04 19:53:53 -0300

    refactor: refactoring adapter and adding request params

--- app/main/adapters/fast_api_adapter.py
+++ app/main/adapters/fast_api_adapter.py
@@ -4,14 +4,22 @@ from app.domain.usecases import UseCase
 from app.services.helpers.http import HttpResponse
 
 
-def fast_api_adapter(request: Union[Any, None], usecase: UseCase) -> HttpResponse:
-    """Adapts a FastAPI request to a usecase
+def fast_api_adapter(
+    factory: UseCase,
+    body: Union[Any, None] = None,
+    query: Union[Any, None] = None,
+    path: Union[Any, None] = None,
+) -> HttpResponse:
+    """Adapter that passes the factory parameters passed
+    to the use case and returns the status code and response body.
 
     Args:
-        request (Any or None): Request to be adapted
-        usecase (Usecase): Usecase to be adapted
+        factory (UseCase): Factory UseCases
+        body (Union[Any, None], optional): Request Body Params. Defaults to None.
+        query (Union[Any, None], optional): Request Query Params. Defaults to None.
+        path (Union[Any, None], optional): Request Path Params. Defaults to None.
 
     Returns:
-        HttpResponse: Response from the usecase
+        HttpResponse: Object with status_code and body
     """
-    return usecase.handle(request)
+    return factory.handle(body, query, path)
